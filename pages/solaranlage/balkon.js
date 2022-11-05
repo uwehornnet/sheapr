1afe974c6faba251b952f608be1ac50e8d6a71bb
uhorn <uwehornnet@users.noreply.github.com>
2022-11-05 21:11:52 +0100

    changes: ebay url params

--- pages/solaranlage/balkon.js
+++ pages/solaranlage/balkon.js
@@ -37,7 +37,7 @@ const FAQ_DATA = [
 
 const QUERY_VARS =
 	process.env.NODE_ENV === "production"
-		? "?mkcid=1&mkrid=707-53477-19255-0&siteid=77&campid=5338914947&customid=&toolid=10001&mkevt=1"
+		? "?amdata=enc%3AAQAHAAAAoD6xcFatNtZNTBC99reD4H2PpIaEZfJAHB%2BVed0PKMeFnIv6eRphAQBBPBr4WBw22L3ONvwBYBpJhJqx3MAH1pweDz%2BNq%2FdP1YKN2kESdAhgmAtFomnwD85dTTXdBoBMn2L96NxclXvVkdN1rXVkkjGR7o2XI5ymZiLWkY9%2Bey7O8N4ZxIAEqmKUs0j2lIdiBfUx0kJf9Q9%2BrW0mCrt%2Bku8%3D&mkcid=1&mkrid=707-53477-19255-0&siteid=77&campid=5338952852&customid=&toolid=10001&mkevt=1"
 		: "";
 
 export default function FuerBalkon() {
