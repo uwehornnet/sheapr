dfdb1144449ddca6e1b24a773320c0d0e62de1fe
uhorn <uwehornnet@users.noreply.github.com>
2023-02-14 09:36:25 +0200

    bugfixes: styling imprint and privacy

--- pages/imprint.js
+++ pages/imprint.js
@@ -2,19 +2,15 @@ import HeadMeta from "../components/HeadMeta";
 
 export default function Imprint() {
 	return (
-		<div className="bg-white w-full min-h-screen">
+		<div className="bg-white w-full">
 			<HeadMeta title="Imprint - Terms of usage" description="Imprint - Terms of usage" />
 
-			<div className="container mx-auto py-32">
+			<div className="container mx-auto pt-32">
 				<h1 className="text-biggest text-center font-semibold">Imprint</h1>
 			</div>
 
-			<main className="container mx-auto py-16">
-				<img
-					alt="Imprint"
-					src="/images/imprint.jpg"
-					className="w-full h-auto object-contain md:aspect-auto min-h-screen md:min-h-0"
-				/>
+			<main className="container mx-auto py-16 px-4">
+				<img alt="Imprint" src="/images/imprint.jpg" className="w-full h-auto object-contain md:aspect-auto" />
 			</main>
 		</div>
 	);
--- pages/privacy.js
+++ pages/privacy.js
@@ -8,16 +8,12 @@ export default function Privacy() {
 				description="The following gives a simple overview of what happens to your personal information when you visit our website."
 			/>
 
-			<div className="container mx-auto py-32">
+			<div className="container mx-auto pt-32">
 				<h1 className="text-biggest text-center font-semibold">Privacy</h1>
 			</div>
 
-			<main className="container mx-auto my-16">
-				<img
-					alt="Privacy"
-					src="/images/privacy.jpg"
-					className="w-full h-auto object-contain md:aspect-auto min-h-screen md:min-h-0"
-				/>
+			<main className="container mx-auto my-16 px-4">
+				<img alt="Privacy" src="/images/privacy.jpg" className="w-full h-auto object-contain md:aspect-auto" />
 			</main>
 		</div>
 	);
