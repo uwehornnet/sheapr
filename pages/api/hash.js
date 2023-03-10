a8869b39b61ddef0ccaea74257b7b1dd2785617a
uhorn <uwehornnet@users.noreply.github.com>
2023-03-10 10:10:09 +0200

    changes: added ebay notification endpoint

--- pages/api/hash.js
+++ pages/api/hash.js
@@ -3,8 +3,7 @@ const { createHash } = require("node:crypto");
 export default async function handler(req, res) {
 	try {
 		const challengeCode = req.query.challenge_code;
-		const token =
-			"b4ecf7e345ce4e06f95db21ca97ce35632d974aa97e8609cba5d1bc785465ba9220b85ecda7e3fa414081706e8e2dd3ffdc3643ba119122936830abc897b22fc";
+		const token = "b4ecf7e345ce4e06f95db21ca97ce35632d974aa";
 		const endpoint = "https://sheapr.vercel.app/api/hash";
 		const hash = createHash("sha256");
 		hash.update(challengeCode);
