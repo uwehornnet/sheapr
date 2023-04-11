66f49bee99f55e156288b171e5ee525deb59944a
uhorn <uwehornnet@users.noreply.github.com>
2023-04-11 12:15:26 +0200

    updates query vars

--- utils/QueryVars.js
+++ utils/QueryVars.js
@@ -2,6 +2,6 @@ const mkrid = "707-53477-19255-0";
 const toolid = "10049";
 const campid = "5338970200";
 const oldValue =
-	"?amdata=enc%3AAQAHAAAAoD6xcFatNtZNTBC99reD4H2PpIaEZfJAHB%2BVed0PKMeFnIv6eRphAQBBPBr4WBw22L3ONvwBYBpJhJqx3MAH1pweDz%2BNq%2FdP1YKN2kESdAhgmAtFomnwD85dTTXdBoBMn2L96NxclXvVkdN1rXVkkjGR7o2XI5ymZiLWkY9%2Bey7O8N4ZxIAEqmKUs0j2lIdiBfUx0kJf9Q9%2BrW0mCrt%2Bku8%3D&mkcid=1&mkrid=707-53477-19255-0&siteid=77&campid=5338952852&customid=&toolid=10001&mkevt=1";
+	"?amdata=enc%3AAQAHAAAAoC%2FKdmLO5RmB5suJpnQoBlL4NVJibkzSKeiVqyKuRcZf0temRDha3HRbfXguIqYcqfPekQolFdRu2vKUJ1DZGzCUuJ6MJ88MTSVGDBx%2B6IPBCV813cgxx34kRmRmQ6q09asxfRE%2FwW9kH%2FQeBN1Ys%2BEnstN9sXQo1jb8NCG8s1nJsqaIyq0qbINHZ2Apja%2B6H6aAn0Y4iW2i%2B8NAEUcnMDg%3D&mkcid=1&mkrid=707-53477-19255-0&siteid=77&campid=5338980638&customid=&toolid=10001&mkevt=1";
 export const QUERY_VARS = oldValue;
-//export const QUERY_VARS = process.env.NODE_ENV === "production" ? `?var=0&mkevt=1&mkcid=1&mkrid=${mkrid}&campid=${campid}&toolid=${toolid}`: "";
+//export const QUERY_VARS = process.env.NODE_ENV === "production" ? `?var=0&mkevt=1&mkcid=1&mkrid=${mkrid}&campid=${campid}&toolid=${toolid}`: ""; //
